7973c1fcd309de19d1d305c4e7cb5b7f54ccabcc
fajarync <fajaryusufnurcahyo19@gmail.com>
2024-05-17 01:23:23 +0700

    first commit

---
+++ contactUs.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>OG Tech - Contact US</title>
+  <?php include "header.php"; ?>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+  <h1 class="page-title grid background-overlay" style="margin:1px;padding-top: 140px;padding-bottom: 140px;">Contact Us</h1>
+    <div class="grid" style="margin:0px 15% 0px 15%">
+        <div id="map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m24!1m12!1m3!1d991.5269867349563!2d106.85603401945035!3d-6.249504366473182!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m9!3e9!4m3!3m2!1d-6.249439199999999!2d106.85668539999999!4m3!3m2!1d-6.2494271999999995!2d106.8566652!5e0!3m2!1sid!2sid!4v1715685571613!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <div id="contactdetailscontainer">
+            <div class="contactdetails">
+                <div class="iconcontainer">
+                    <i class="medium material-icons" style="font-size: 65px;">place</i>
+                </div>
+                <p class="grey-text text-lighten-4 bold" style="margin:8px;font-size:16px">
+                    <strong class="grey-text text-lighten-4 bold" style="font-size:22px">OG Solutions</strong><br>
+                    Jakarta Selatan, RT.1/RW.4, Pengadegan, Kec. Pancoran, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12770
+                </p>
+            </div>
+            <div class="contactdetails">
+                <div class="iconcontainer">
+                    <i class="medium material-icons" style="font-size: 65px;">phone</i>
+                </div>
+                <p class="grey-text text-lighten-4 bold" style="margin:8px;font-size:22px">
+                    <strong class="grey-text text-lighten-4 bold" style="font-size:23px">Contact Number</strong><br>
+                   089637506226
+                </p>
+            </div>
+            <div class="contactdetails">
+                <div class="iconcontainer">
+                    <i class="medium material-icons" style="font-size: 65px;">email</i>
+                </div>
+                <p class="grey-text text-lighten-4 bold" style="margin:8px;font-size:16px">
+                    <strong class="grey-text text-lighten-4 bold" style="font-size:20px">OG Solutions</strong><br>
+                    fajaryusufnurcahyo26@gmail.com
+                </p>
+            </div>
+            <div class="contactdetails">
+                <div class="iconcontainer">
+                    <i class="medium material-icons" style="font-size: 65px;">alarm</i>
+                </div>
+                <p class="grey-text text-lighten-4 bold" style="margin:8px;font-size:16px">
+                    <strong class="grey-text text-lighten-4 bold" style="font-size:20px">Business Hours</strong><br>
+                    Monday to Sunday: 11.00am to 8.00pm
+                </p>
+            </div>
+        </div>
+    </div>  
+
+
+  <?php require "footer.php"; ?>
+</body>
+</html>
